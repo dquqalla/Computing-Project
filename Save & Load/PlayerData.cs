9848445d3eba1587a64a21936001d1b5bb3c4b61
Dardan <61984924+dquqalla@users.noreply.github.com>
2020-08-12 19:07:24 +0100

    Add files via upload

---
+++ Save & Load/PlayerData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Credit to Brackey's tutorial for providing guidance on how to create a save system (Link: https://youtu.be/XOjd_qU2Ido). 
+// [System.Serializable] allows for the data below to be converted into bytes to be stored onto the user's device. 
+[System.Serializable]
+public class PlayerData
+{
+    // References to variables which will store the data of the user's current game. 
+    public int level;
+    public int currentScore;
+    public List<string> stars;
+    public float[] position; 
+
+    // Create object PlayerData which will be used to create a saved file with the user's relevant data when they save their game progress. 
+    public PlayerData (Player player)
+    {
+        // Assign each variable with the values of the player, i.e. the current level they are on, their current score, stars completed, and position. 
+        level = player.level;
+        currentScore = player.currentScore;
+        stars = player.stars;
+
+        // Initiliase the variable poisiton to a float array of size 3. This will contain the user's x, y, and z coordinates in-game. 
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
---
+++ Save & Load/SaveSystem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// Credit to Brackey's tutorial for providing guidance on how to create a save system (Link: https://youtu.be/XOjd_qU2Ido). 
+// This script is the save system used to create BinaryFormatter and FileStream objects used to stream data from the game and convert it into binary to be stored on the user's device. 
+public static class SaveSystem
+{
+    // Function used to save the player's current game. 
+    public static void SavePlayer (Player player)
+    {
+        // Create a new BinaryFormatter object.
+        BinaryFormatter formatter = new BinaryFormatter();
+        // Define the path that will store the saved file. The use of "Application.persistenDataPath" will ensure the game can be saved on any device regardless if it is Windows or MacOS.
+        string path = Application.persistentDataPath + "/player.speech";
+        // Create a new FileStream object which will read the user's data and create a new file using "FileMode.Create".
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        // Create a new PlayerData object data. This will contain the saved file data.
+        PlayerData data = new PlayerData(player);
+
+        // Call the binary formatter to stream the data from the FileStream object into the PlayerData object.
+        formatter.Serialize(stream, data);
+        // Close the stream and save the file to the user's device.
+        stream.Close();
+    }
+
+    // Function used to load the user's game.
+    public static PlayerData LoadPlayer ()
+    {
+        // Define the path of the user's saved file.
+        string path = Application.persistentDataPath + "/player.speech";
+
+        // If a file exists in that specified path...
+        if (File.Exists(path))
+        {
+            // Create a new BinaryFormatter and FileStream object to read the file.
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            // Create a PlayerData object and assign it the data of the saved file.
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            // Close the stream.
+            stream.Close();
+            
+            // Return the saved data.
+            return data;
+
+        } 
+        // Else...
+        else
+        {
+            // Return null as no saved file exists.
+            return null;
+        }
+    }
+
+    // Function called to delete a saved file whenever a user stars a new game.
+    public static void Delete()
+    {
+        File.Delete(Application.persistentDataPath + "/player.speech");
+    }
+}
