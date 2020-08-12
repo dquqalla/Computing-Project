82a15a7824028414c39985d03bb4b3c9af2dce6a
Dardan <61984924+dquqalla@users.noreply.github.com>
2020-08-12 19:59:47 +0100

    Add files via upload

---
+++ Other/Autosave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Autosave : MonoBehaviour
+{
+    public RectTransform rectComponent;
+    private float rotateSpeed = 200f;
+
+    private void Update()
+    {
+        rectComponent.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+
+    public void ActivateIcon()
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(RemoveAfterSeconds(3));
+    }
+
+    IEnumerator RemoveAfterSeconds(int seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Other/LastScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LastScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<AudioManager>().Play("PartyPopperSFX");
+        FindObjectOfType<AudioManager>().Play("BackgroundMusic");
+    }
+}
